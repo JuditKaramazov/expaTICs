fe996c4f692b2893a943b884f84b2b0dd08ee940
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-22 23:55:24 +0200

    Add Support.jsx and chat

---
+++ app/api/chat/route.js
@@ -0,0 +1,35 @@
+// app/api/chat/route.js
+
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+export const runtime = 'edge'
+
+const apiConfig = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(apiConfig)
+
+export async function POST(req) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Request the OpenAI API for the response based on the prompt
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: messages,
+    max_tokens: 500,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  })
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}
--- src/pages/Dashboard/Support/Support.jsx
+++ src/pages/Dashboard/Support/Support.jsx
@@ -5,18 +5,22 @@ export default function Support() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="mx-auto w-full max-w-md py-24 flex flex-col">
-      {messages.length > 0 &&
-        messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap">
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      {
+        messages.map(message => {
+          return (
+          <div key={message.id} className="whitespace-pre-wrap">
+            {message.role === "user" ? "User: " : "AI: "}
+            {message.content}
           </div>
-        ))}
+        )
+      })
+    }
 
       <form onSubmit={handleSubmit}>
         <input
           value={input}
+          className="fixed w-full max-w-md bottom-0 border border-gray"
           placeholder="What can I help you with?"
           onChange={handleInputChange}
         />
