3b15d4fde09335175a7257e0a603e74ffc821d25
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-23 18:44:48 +0200

    Fix minor problems

--- src/pages/Dashboard/Support/Support.jsx
+++ src/pages/Dashboard/Support/Support.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 import { useChat } from "ai/react";
+
+const Container = styled.div`
+width: 100%;
+max-width: 25rem;
+display: flex;
+flex-direction: column;
+margin-left: auto;
+margin-right: auto;
+align-items: stretch;
+padding-top: 6rem;
+padding-bottom: 6rem;
+
+input {
+  width: 100%;
+  max-width: 36rem;
+  margin: auto;
+  margin-bottom: 2rem;
+  position: fixed;
+  bottom: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  border: 1px solid #cbd5e0;
+  border-radius: 20px;
+  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+}
+`;
 
 export default function Support() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+    <Container>
       {
         messages.map(message => {
           return (
-          <div key={message.id} className="whitespace-pre-wrap">
+          <div key={message.id} className="message-box">
             {message.role === "user" ? "User: " : "AI: "}
             {message.content}
           </div>
@@ -20,12 +49,10 @@ export default function Support() {
       <form onSubmit={handleSubmit}>
         <input
           value={input}
-          className="fixed w-full max-w-md bottom-0 border border-gray"
           placeholder="What can I help you with?"
           onChange={handleInputChange}
         />
-        <button type="submit">Send</button>
       </form>
-    </div>
+    </ Container>
   );
 }
