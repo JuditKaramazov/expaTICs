3ad8cb4a2c09daeb8e4f9f9bffba1973c2e1a07b
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-25 23:29:49 +0200

    Improve CoffeeGift modal

--- src/components/CoffeeGift/CoffeeGift.jsx
+++ src/components/CoffeeGift/CoffeeGift.jsx
@@ -1,17 +1,19 @@
+import { FaRegPaperPlane } from 'react-icons/fa';
 import { coffeeTypes, messageTemplates } from "@/src/utils/coffeeData";
 
 const CoffeeGift = () => {
-    return (
-        <div id="modal">
-          <div className="coffee-gift">
-            <h1 id="message">Hey, dear:</h1>
-            <h2> Here's a cup of</h2>
-            <img src="/coffeeTemplate.png" style={{width: "150px"}} alt="Cup of coffee visual." />
-            <h3>{coffeeTypes[Math.floor(Math.random() * coffeeTypes.length)]}</h3>
-            <q className="description">{messageTemplates[Math.floor(Math.random() * messageTemplates.length)]}</q>
-          </div>
-        </div>
-    );
+  return (
+    <div id="modal">
+      <div className="coffee-gift">
+        <h1 style={{fontSize: "2.2rem"}}>Hey, dear: </h1>
+        <FaRegPaperPlane size={48} />
+        <h2> Here's a cup of</h2>
+        <img src="/coffeeTemplate.png" style={{ width: "150px" }} alt="Cup of coffee visual." />
+        <h3>{coffeeTypes[Math.floor(Math.random() * coffeeTypes.length)]}</h3>
+        <q className="description">{messageTemplates[Math.floor(Math.random() * messageTemplates.length)]}</q>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeGift;
