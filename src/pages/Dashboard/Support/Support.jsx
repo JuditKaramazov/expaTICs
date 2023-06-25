3852af141ee589000af08c65518d110d2fa7a3b0
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-25 02:30:42 +0200

    Add Support styles

--- src/pages/Dashboard/Support/Support.jsx
+++ src/pages/Dashboard/Support/Support.jsx
@@ -1,34 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import { Container } from "./Support.styled";
 import { useChat } from "ai/react";
-
-const Container = styled.div`
-width: 100%;
-max-width: 25rem;
-display: flex;
-flex-direction: column;
-margin-left: auto;
-margin-right: auto;
-align-items: stretch;
-padding-top: 6rem;
-padding-bottom: 6rem;
-
-input {
-  width: 100%;
-  max-width: 36rem;
-  margin: auto;
-  margin-bottom: 2rem;
-  position: fixed;
-  bottom: 1rem;
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  border: 1px solid #cbd5e0;
-  border-radius: 20px;
-  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
-}
-`;
 
 export default function Support() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
