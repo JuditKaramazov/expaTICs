2a7c83bbeeeddfe68cbfdd0a50f3a9ff226c629c
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-06-26 05:34:39 +0200

    Improve Support.jsx

--- src/pages/Dashboard/Support/Support.jsx
+++ src/pages/Dashboard/Support/Support.jsx
@@ -1,30 +1,43 @@
-import React from "react";
-import { Container } from "./Support.styled";
+import React, { useState } from "react";
+import { Section, Introduction, Container, MessageBox } from "./Support.styled";
+import { MdHelpOutline } from "react-icons/md";
+import { Modal } from "@/src/components/Modal/Modal";
+import ChatHelp from "@/src/components/ChatHelp/ChatHelp";
 import { useChat } from "ai/react";
 
 export default function Support() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <Container>
-      {
-        messages.map(message => {
-          return (
-          <div key={message.id} className="message-box">
-            {message.role === "user" ? "User: " : "AI: "}
+    <Section>
+      <Introduction>
+        <h1>Sometimes, support</h1>
+        <h2>is something that we require from the outside</h2>
+        <p>and can't be found deep inside ourselves.</p>
+        <p>International health insurance, visa types, or knowing 
+          where to go often feel like immense obstacles.</p>
+      </ Introduction>
+      <Container>
+        {messages.map((message) => (
+          <MessageBox key={message.id} isUser={message.role === "user"}>
             {message.content}
-          </div>
-        )
-      })
-    }
-
-      <form onSubmit={handleSubmit}>
-        <input
-          value={input}
-          placeholder="What can I help you with?"
-          onChange={handleInputChange}
-        />
-      </form>
-    </ Container>
+          </MessageBox>
+        ))}
+        <form onSubmit={handleSubmit}>
+          <input
+            value={input}
+            placeholder="What can I help you with?"
+            onChange={handleInputChange}
+          />
+          <span className="icon" onClick={() => setShowModal(true)}>
+            <MdHelpOutline />
+          </span>
+        </form>
+      </ Container>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <ChatHelp />
+      </ Modal>
+    </ Section>
   );
 }
--- src/pages/Dashboard/Support/Support.styled.js
+++ src/pages/Dashboard/Support/Support.styled.js
@@ -1,5 +1,78 @@
 import styled from "styled-components";
 
+export const Section = styled.section`
+  min-height: calc(100vh - ${(props) => props.theme.navHeight});
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  margin-left: 5rem;
+  margin-right: 1rem;
+  position: relative;
+  animation: smoothStart 2s ease-in;
+
+  @keyframes smoothStart {
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+
+  @media (max-width: 30em) {
+    width: 95%;
+    margin: 0 30px 0 30px;
+    font-size: ${(props) => props.theme.fontsm};
+  }
+`;
+
+export const Introduction = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 1rem;
+
+  h2 {
+    display: inline-block;
+    margin-left: 6px;
+    margin-bottom: 4px;
+    position: relative;
+  }
+
+  h2::after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: #010101;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+  
+  h2:hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
+
+  p {
+    margin-left: 6px;
+  }
+
+  @media (max-width: 30em) {
+    width: 90%;
+    margin-left: 50px;
+    font-size: ${(props) => props.theme.fontsm};
+  }
+`;
+
 export const Container = styled.div`
   width: 100%;
   max-width: 25rem;
@@ -10,20 +83,60 @@ export const Container = styled.div`
   align-items: stretch;
   padding-top: 6rem;
   padding-bottom: 6rem;
-  
+
+  .input-container {
+    position: fixed;
+    bottom: 0.2rem;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: calc(100% - 1rem);
+    max-width: 36rem;
+    margin-bottom: 1rem;
+    z-index: 1;
+  }
+
   input {
     width: 100%;
-    max-width: 36rem;
-    margin: auto;
-    margin-bottom: 2rem;
-    position: fixed;
-    bottom: 1rem;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
+    outline: none;
     border: 1px solid #cbd5e0;
     border-radius: 20px;
+    padding: 0.5rem 1rem;
     box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
   }
+
+  .icon {
+    width: 2.5rem;
+    height: 2.5rem;
+    margin-left: -3rem;
+    margin-top: -2.3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .icon svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    color: darkgray;
+
+    &:hover {
+      color: pink;
+    }
+  }
+`;
+
+export const MessageBox = styled.div`
+  max-width: 70%;
+  margin: 0.5rem;
+  padding: 1rem;
+  align-self: ${(props) => (props.isUser ? "flex-end" : "flex-start")};
+  background-color: ${(props) => (props.isUser ? "#d1e5f1" : "#fdfbe8")};
+  border-radius: 10px;
+  border: 1px solid ${(props) => (props.isUser ? "#6faed9" : "#e6e6e6")};
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 `;
